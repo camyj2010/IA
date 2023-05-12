fd05cb5fc2abb1acc4d0b686cf240995def06983
Valery Molina <valerymolinalika@gmail.com>
2023-05-11 20:29:50 -0500

    Proyecto finalizado

--- interfaz.py
+++ interfaz.py
@@ -16,12 +16,28 @@ from bi_Greedy_Search import Greedy_Search
 from bi_A_Star_Search import A_Star_Search
 pygame.init()
 
-#Load resource
+#------------------------------------------------CARGAR RECURSOS DEL JUEGO---------------------------------------------------------------------------------#
+#Se cargan los recursos como imagenes y audios 
 SCREEN = pygame.display.set_mode((1280,720))
 BG=pygame.image.load("source\imagenes\original.jpg")
 dragon=pygame.transform.scale(pygame.image.load("source\imagenes\dragon_resize.png"), (int(250), int(200)))
 ball=pygame.transform.scale(pygame.image.load("source\imagenes\dragon_ball.png"), (int(62), int(62)))
 goku_tabien=pygame.transform.scale(pygame.image.load("source\imagenes\goku_tabien.jpg"), (int(380), int(253)))
+
+# Obstaculos
+obstacle = pygame.transform.scale(
+    pygame.image.load("source\imagenes\obstacle.png"), (int(62), int(62))
+)
+
+# Semilla
+bean = pygame.transform.scale(
+    pygame.image.load("source\imagenes\seed.png"), (int(62), int(62))
+)
+
+# Cloud
+cloud = pygame.transform.scale(
+    pygame.image.load("source\imagenes/nube.png"), (int(62), int(62))
+)
 
 sound_mixer = pygame.mixer.Channel(5)
 sound = pygame.mixer.Sound("source\sounds\menu.ogg")
@@ -30,11 +46,13 @@ sound.set_volume(0.1)
 sound2 = pygame.mixer.music
 sound2.load("source\sounds\play.mp3")
 sound2.set_volume(0.1)
-
+#---------------------------------------------------------------------------------------------------------------------------------#
+#Dimenciones de la pantalla 
 width=1280
 height=720
 window_=pygame.display.set_mode((width,height))
 
+#----------------------------------- SPRITES PARA LAS ANIMACIONES ----------------------------------------------------------------#
 #Frieza sprite
 sprite_sheet_image = pygame.image.load("source/imagenes/frieza.png")
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
@@ -62,6 +80,7 @@ for i in range(goku_animation_steps):
     goku_frame = goku_sprite_sheet.get_image(i, 62, 62, 1, (255, 255, 255))
     goku_animation_frames.append(goku_frame)
 
+#---------------------------------------------- RECURSOS DEL MAPA  ----------------------------------------------------------------#
 # Obstaculos
 obstacle = pygame.transform.scale(
     pygame.image.load("source\imagenes\obstacle.png"), (int(62), int(62))
@@ -77,18 +96,23 @@ cloud = pygame.transform.scale(
     pygame.image.load("source\imagenes/nube.png"), (int(62), int(62))
 )
 
+#-------------------------------------------- UTILIDADES ----------------------------------------------------------------------------#
+# Funcion que retorna una fuente de letra de PyGame 
 def get_font(size, number): # Returns Press-Start-2P in the desired size
     if(number==1):
         return pygame.font.Font("source\saiyan-sans_font\Saiyan-Sans.ttf", size)
     if(number==2):
         return pygame.font.Font("source\saiyan-sans_font\ChicagoFLF.ttf", size)
+#
 
+# Funcion que dibuja un texto
 def draw_text(x, y, string, col, size, window, number=1):
     font = get_font(size,number)
     text = font.render(string, True, col)
     textbox = text.get_rect(center=(x,y))
     window.blit(text, textbox)
-
+    
+#Funcion que Lee el mapa seleccionado
 def read_map(file_name):
     map = []
     with open("source\map\{}".format(file_name), "r") as file:
@@ -97,33 +121,24 @@ def read_map(file_name):
             str = [int(i) for i in str]
             map.append(str)
     return map
-
-map=[
-[0, 5, 3, 1, 1, 1, 1, 1, 1, 1],
-[0, 1, 0, 0, 1, 0, 0, 0, 1, 1],
-[0, 1, 1, 0, 3, 5, 1, 0, 2, 0],
-[0, 1, 1, 1, 3, 1, 1, 1, 1, 0],
-[6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[1, 1, 4, 1, 1, 1, 1, 1, 1, 0],
-[1, 1, 0, 4, 4, 0, 0, 1, 1, 5],
-[1, 1, 0, 0, 1, 1, 0, 1, 1, 0],
-[0, 0, 0, 0, 1, 1, 5, 0, 0, 0],
-[1, 1, 1, 0, 1, 1, 6, 1, 1, 1]]
-
+#-----------------------------------------------------------PESTAÑAS DEL JUEGO--------------------------------------------------------------------------------------
+#Funcion de la pestaña del Juego 
 def play():
-    # sound.stop()
-    # sound.play()
+    
     #Animation variables
+    
     frame = 0
     cell_frame = 0
     frame_rate = 3
     last_update = pygame.time.get_ticks()
    
+   # Colores de los Dropdown
     COLOR_INACTIVE = (135, 206, 235)
     COLOR_ACTIVE = (100, 200, 255)
     COLOR_LIST_INACTIVE = (135, 206, 235)
     COLOR_LIST_ACTIVE = (255, 150, 150)
     
+    # Dropdown de seleccion de mapa
     file_names = [f for f in os.listdir("source\map") if f.endswith(".txt")]
     DROPDOWN = DropDown(
         [COLOR_INACTIVE, COLOR_ACTIVE],
@@ -131,7 +146,8 @@ def play():
         950, 50, 200, 50, 
         pygame.font.SysFont(None, 30), 
         "Select Map", file_names)
-
+  
+    # Dropdown de seleccion de tipo de busqueda (informada o no informada)
     DROPDOWN_SEARCH = DropDown(
         [COLOR_INACTIVE, COLOR_ACTIVE],
         [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
@@ -139,6 +155,7 @@ def play():
         pygame.font.SysFont(None, 30), 
         "Select Search", ["Informada", "No Informada"])
     
+    # Dropdown de seleccion de algoritmo
     DROPDOWN_ALG = DropDown(
         [COLOR_INACTIVE, COLOR_ACTIVE],
         [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
@@ -149,6 +166,7 @@ def play():
     INFORMADA = ['Avara', 'A*']
     NO_INFORMADA = ['Amplitud', 'Costo Uniforme', 'Profundidad']
 
+    # ----------------------- VARIABLES PARA ALMACENAR COSAS VARIAS EN EL JUEGO --------------------------- #
     map = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -163,15 +181,15 @@ def play():
         ]
     
 
-    solution = []
-    maps = []
-    traveled = []
+    solution = []   # Solucion de un problema
+    maps = []       # Mapas para actualizar las soluciones
+    traveled = []   # Recorrido de Goku
 
-    enlapsed_time = ""
-    tree_depth = ""
-    expanded_nodes = ""
-    total_cost = ""
-    count = 0
+    enlapsed_time = ""      # Tiempo
+    tree_depth = ""         # Profundidad del arbol
+    expanded_nodes = ""     # Cantidad de nodos expandidos
+    total_cost = ""         # Costo de la solucion
+    count = 0               # -> Para evitar que se repita un algoritmo en progreso
 
     while True:
 
@@ -179,7 +197,8 @@ def play():
 
         SCREEN.fill("black")
         SCREEN.blit(BG, (0, 0)) 
-
+        
+        #Textos del resultados y seleccion
         PLAY_TEXT = get_font(38,2).render("Maps:", True, "White")
         PLAY_TEXT2 = get_font(38,2).render("Search:", True, "White")
         PLAY_TEXT3 = get_font(38,2).render("Algoritm:", True, "White")
@@ -195,6 +214,7 @@ def play():
         #RECT=pygame.draw.rect(window_,"White",(80,50,620,620) )
         #RECT_2=pygame.draw.rect(window_,"White",(750,50,350,530) )
 
+        #Campos de entrada
         PLAY_RECT = PLAY_TEXT.get_rect(center=(815, 70))
         PLAY_RECT2 = PLAY_TEXT.get_rect(center=(815, 150))
         PLAY_RECT3 = PLAY_TEXT.get_rect(center=(815, 230))
@@ -208,6 +228,7 @@ def play():
         PLAY_RECT11 = PLAY_TEXT.get_rect(center=(1090, 482))
         PLAY_RECT12 = PLAY_TEXT.get_rect(center=(1090, 582))
 
+        #Poscionamiento de los textos  
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
         SCREEN.blit(PLAY_TEXT2, PLAY_RECT2)
         SCREEN.blit(PLAY_TEXT3, PLAY_RECT3)
@@ -221,7 +242,7 @@ def play():
         SCREEN.blit(PLAY_TEXT11, PLAY_RECT11)
         SCREEN.blit(PLAY_TEXT12, PLAY_RECT12)
 
-
+        #Botones para devolverse, solucionar y reiniciar
         PLAY_BACK = Button(pygame.transform.scale(pygame.image.load("source\imagenes\Play_Rect_4.png"), (int(200), int(150))), pos=(1090, 650), 
                             text_input="BACK", font=get_font(40,1), base_color="#FFFFFF", hovering_color="#87CEEB")
 
@@ -245,18 +266,23 @@ def play():
         DROPDOWN.draw(SCREEN)
 
 
-        
+        # ESCUCHAS DE EVENTOS DE BOTONES
         event_list = pygame.event.get()
         for event in event_list:
+            # Exit
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == pygame.MOUSEBUTTONDOWN:
+                # Boton de back
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+
+                # Boton de resolver
                 if PLAY_SOLVE.checkForInput(PLAY_MOUSE_POS):
                     
-                    #sound_mixer.play(sound2)
+                    # -------------------------------- ALGORITMO DE BUSQUEDA POR AMPLITUD ------------------------------------- #
                     if DROPDOWN_ALG.main == "Amplitud" and count==0:
                         print('Amplitud')
                         start=time.time()
@@ -274,7 +300,8 @@ def play():
                         count += 1
 
                         solution += path
-
+                        
+                    # ---------------------------- ALGORITMO DE BUSQUEDA POR COSTO UNIFORME ------------------------------------- #
                     elif DROPDOWN_ALG.main == "Costo Uniforme" and count==0:
                         print('Costo Uniforme')
                         start=time.time()
@@ -292,7 +319,7 @@ def play():
                         count += 1
 
                         solution += path
-                        
+                    #---------------------------- ALGORIMO DE BUSQUEDA POR PROFUNDIDAD--------------------------------------------- # 
                     elif DROPDOWN_ALG.main == "Profundidad" and count==0:
                         print('Profundidad')
                         start=time.time()
@@ -309,7 +336,8 @@ def play():
                         count += 1
 
                         solution += path
-                        
+                    
+                    # -------------------------------- ALGORITMO DE BUSQUEDA A ESTRELLA -------------------------------------- #
                     elif DROPDOWN_ALG.main == "A*" and count==0:
                         print('A*')
                         start=time.time()
@@ -328,6 +356,7 @@ def play():
 
                         solution += path
 
+                    # ------------------------------------- ALGORITMO DE BUSQUEDA AVARA -------------------------------------- #
                     elif DROPDOWN_ALG.main == "Avara" and count==0:
                         print('Avara')
                         start=time.time()
@@ -351,8 +380,11 @@ def play():
                         sound_mixer.stop()
                         sound2.play(start=10.1)
 
+                # Boton de reinicio
                 if PLAY_RESTART.checkForInput(PLAY_MOUSE_POS):
                     
+                    # Se reinician todas las variables a su valor 
+                    # por defecto y se vuelve a leer el mapa
                     if DROPDOWN.main != 'Select Map':
                         solution = []
                         traveled = []
@@ -364,12 +396,14 @@ def play():
                         map = read_map(DROPDOWN.main)
 
             
+        # Actualizar el dropdown de mapas
         selected_option = DROPDOWN.update(event_list)
         if selected_option >= 0:
             DROPDOWN.main = DROPDOWN.options[selected_option]
             map = read_map(DROPDOWN.main)
             count = 0
 
+        # Actualizar el dropdown de seleccion del tipo de busqueda
         selected_search = DROPDOWN_SEARCH.update(event_list)
         if selected_search >= 0:
             DROPDOWN_SEARCH.main = DROPDOWN_SEARCH.options[selected_search]
@@ -379,32 +413,30 @@ def play():
             elif selected_search == 1:
                 DROPDOWN_ALG.update_options(NO_INFORMADA)
 
+        # Actualizar el dropdown del tipo de algoritmo 
         selected_alg = DROPDOWN_ALG.update(event_list)
         if selected_alg >= 0:
             count=0
             DROPDOWN_ALG.main = DROPDOWN_ALG.options[selected_alg]
        
-        
-
-            
-
-        if len(solution) > 0:
+        # Actualizar el mapa con el recorrido de goku
+        if len(solution) > 0: # Verificar que haya solucion
             pos = solution[0]
             
             map = maps[0]
 
+            # Eliminar el 2
             for i in range(len(map)):
                 for j in range(len(map)):
                     if map[i][j] == 2:
                         map[i][j] = 0
             
-            #print(pos)
+            # Actualizar la nueva posicion de Goku
             map[pos[0]][pos[1]] = 2
             
-            # print(map)
-            # print()
             time.sleep(0.3)
 
+            # Actualizar las nubes del recorrido de Goku
             for tile in traveled:
                 if map[tile[0]][tile[1]] == 0:
                     map[tile[0]][tile[1]] = 7
@@ -415,15 +447,15 @@ def play():
             maps = maps[1:]
         
         else:
+            # Reproducion del sonido de pelea
             if sound2.get_busy():
                 sound2.fadeout(1500)
-                #sound_mixer.play(sound)
+
             elif sound_mixer.get_sound() == None:
                 sound_mixer.play(sound)
             
 
-  
-        #update the animation frames
+        # ----------------------------------- DIBUJAR EL MAPA ----------------------------------- #
         current_time = pygame.time.get_ticks()
         if current_time - last_update >= 1000 // frame_rate:
             last_update = current_time
@@ -471,14 +503,11 @@ def play():
                     SCREEN.blit(cloud, (POS_X+80, POS_Y+50))
                 
                 pygame.draw.rect(SCREEN, (0, 0, 0), rect, 1) # Dibuja el borde negro
-
-
-        
-
         
         pygame.display.update()
 
 
+#Funcion de la pestaña de creditos
 def credits():
     # sound.stop()
     # sound.play()
@@ -526,7 +555,7 @@ def credits():
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -537,6 +566,8 @@ def credits():
 
         pygame.display.update()
 
+
+#Funcion de la pestaña de cargar mapas
 def maps():
     # sound.stop()
     # sound.play()
@@ -593,16 +624,16 @@ def maps():
        
         pygame.display.update()
         
-
+#Funcion de la pestaña del menu principal
 def main_menu():
      #sound.stop()
      sound_mixer.play(sound)  
+     
      while True:
-      
+        
         SCREEN.blit(BG, (0, 0)) 
-
         MENU_MOUSE_POS = pygame.mouse.get_pos()
-
+        
         draw_text(695, 125, "Dragon Ball Maze", "Black", 152, SCREEN)
         draw_text(705, 115, "Dragon Ball Maze", "Black", 152, SCREEN)
         draw_text(695, 115, "Dragon Ball Maze", "Black", 152, SCREEN)
@@ -612,7 +643,9 @@ def main_menu():
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
         draw_text(376, 120, "Dragon", "Yellow", 150, SCREEN)
-
+        
+        #Botones de las pantallas 
+        
         PLAY_BUTTON = Button(image=pygame.image.load("source\imagenes\Play_Rect_3.png"), pos=(250, 600), 
                             text_input="PLAY", font=get_font(60,1), base_color="#FFFFFF", hovering_color="#87CEEB")
         
@@ -623,9 +656,9 @@ def main_menu():
                             text_input="MAP", font=get_font(60,1), base_color="#FFFFFF", hovering_color="#87CEEB")
         
         
-
+        
         SCREEN.blit(dragon, (5,30))
-
+        
         for button in [PLAY_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
@@ -655,5 +688,3 @@ def main_menu():
 
 
 main_menu()
-
-
